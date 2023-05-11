11762d943b1aceb3fbe690e9630c082b54d48a5d
Eli <bingeli1379@gmail.com>
2023-05-11 23:36:32 +0800

    feat: add github pages settings

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dynamic-virtual-scroll/',
   plugins: [vue()],
   resolve: {
     alias: {
